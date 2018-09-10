49ee51df2addc49eccb3e0588949db3e49e873a1
Iman Jawad <imanj12@gmail.com>
2018-09-10 20:42:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+# require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  
+  new_hash = {}
+  
+  languages.each do |style, style_attrib| 
+    style_attrib.each do |lang, lang_attrib|
+      new_hash[lang] = lang_attrib
+      if new_hash[lang].include?(:style) == true
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang][:style] = []
+        new_hash[lang][:style] << style
+      end
+    end
+  end
+
+new_hash
+
 end
+
+# reformat_languages(languages)
